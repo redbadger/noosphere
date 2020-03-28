c98e698caa880fbe0b15d0bb9df4b2a4a5b52785
Pedro Martin <pedro.martin@red-badger.com>
2020-03-28 09:47:02 +0000

    set prod config

--- notebook/config/webpack.dev.ts
+++ notebook/config/webpack.dev.ts
@@ -1,10 +1,15 @@
 import webpack from 'webpack';
+import path from 'path';
 import config from './webpack.config';
 
 const devConfig: webpack.Configuration = {
   ...config,
   mode: 'development',
   devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, '../dev-build'),
+    filename: 'dev-build.js',
+  },
   // @ts-ignore
   devServer: {
     open: true,
