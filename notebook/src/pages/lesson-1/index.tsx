e9034f21dbb9a2e1406635fffdbf2b45a933965b
Pedro Martin <pedro.martin@red-badger.com>
2020-03-28 21:40:07 +0000

    Draft proof of concept for slides

---
+++ notebook/src/pages/lesson-1/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const lessonOne: React.FC = () => <h1>Lesson 1</h1>;
+
+export default lessonOne;
---
+++ notebook/src/pages/lesson-2/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const lessonTwo: React.FC = () => <h1>Lesson 2</h1>;
+
+export default lessonTwo;
