43a297871a6bd81f4deb2036918236e416bb65f1
Pedro Martin <pedro.martin@red-badger.com>
2020-03-31 20:06:56 +0100

    Remove lesson package

--- lessons/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export interface Lesson {
-    name: string;
-    slides: string;
-}
-declare const map: Lesson[];
-export default map;
--- lessons/src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as directoriesMap from './directories-map.json';
-
-export interface Lesson {
-  name: string;
-  slides: string;
-}
-
-const map: Lesson[] = directoriesMap;
-export default map;
