42b24190c6bc4fb10f30684cdf03703b5c4727e1
Pedro Martin <pedro.martin@red-badger.com>
2020-04-02 13:39:04 +0100

    Able to change theme defaults

--- notebook/src/components/presentation/index.tsx
+++ notebook/src/components/presentation/index.tsx
@@ -7,18 +7,44 @@ type PresentationContent = {
   content: React.ReactNode;
 };
 
+// const theme = {
+//   colors: {
+//     white: '#fff',
+//     black: '#000',
+//   },
+//   fontSizes: {
+//     header: '64px',
+//     paragraph: '28px',
+//   },
+//   backgroundColor: '#fff',
+// };
 const theme = {
+  size: {
+    width: 1366,
+    height: 768,
+    maxCodePaneHeight: 200,
+  },
   colors: {
-    white: '#fff',
-    black: '#000',
+    primary: 'rgba(0,0,0,.8)',
+    secondary: 'rgb(0,0,0)',
+    tertiary: 'rgb(255, 255, 255)',
+    quaternary: '#ffc951',
+    quinary: '#8bddfd',
+  },
+  fonts: {
+    header: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+    text: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+    monospace: '"Consolas", "Menlo", monospace',
   },
   fontSizes: {
-    header: '64px',
-    paragraph: '28px',
+    h1: '72px',
+    h2: '64px',
+    h3: '56px',
+    text: '44px',
+    monospace: '20px',
   },
-  backgroundColor: '#fff',
+  space: [16, 24, 32],
 };
-
 const Presentation: React.FC<PresentationContent> = ({
   content,
 }: PresentationContent) => (
