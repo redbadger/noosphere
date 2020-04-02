0faaedfd3802ed29bc5cbba65985dd8a2702beda
Pedro Martin <pedro.martin@red-badger.com>
2020-04-02 15:07:56 +0100

    Routes works on dev not prod

--- notebook/src/styles/theme.ts
+++ notebook/src/styles/theme.ts
@@ -1,13 +1,31 @@
+const base = 20;
+
 const theme = {
+  size: {
+    width: 1366,
+    height: 768,
+    maxCodePaneHeight: 200,
+  },
   colors: {
-    white: '#fff',
-    black: '#000',
+    primary: 'rgba(0,0,0,.7)',
+    secondary: 'rgb(0,0,0)',
+    tertiary: 'rgb(255, 255, 255)',
+    quaternary: 'rgba(0,0,0,.8)',
+    quinary: '#8bddfd',
+  },
+  fonts: {
+    header: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+    text: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+    monospace: '"Consolas", "Menlo", monospace',
   },
   fontSizes: {
-    header: '64px',
-    paragraph: '28px',
+    h1: base * 2,
+    h2: base * 1.5,
+    h3: base * 1.17,
+    text: base,
+    monospace: base / 2,
   },
-  backgroundColor: '#fff',
+  space: [base / 2, base, base * 1.5],
 };
 
 export default theme;
