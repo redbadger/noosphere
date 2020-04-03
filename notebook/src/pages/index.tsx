02e8c546766063db2d0f621198a82c8683386eb4
Pedro Martin <pedro.martin@red-badger.com>
2020-04-03 08:48:13 +0100

    Move navigation to index page

--- notebook/src/pages/index.tsx
+++ notebook/src/pages/index.tsx
@@ -1,9 +1,17 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const IndexPage: React.FC = () => (
   <Fragment>
     <h1>Noosphere</h1>
     <h2>Table of contents</h2>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/git">Git & GitHub</Link>
+        </li>
+      </ul>
+    </nav>
   </Fragment>
 );
 
